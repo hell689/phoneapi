1b153b494fd3175edfeb0a82b3ae784c74e03fa3
Alexey Vladyko <a.uladyka@gmail.com>
2020-03-19 16:59:00 +0300

    AddPhone Form

--- PhoneApi/App/containers/phone.jsx
+++ PhoneApi/App/containers/phone.jsx
@@ -2,7 +2,8 @@
 
 export default class Phone extends React.Component {
     state = {
-        phones: []//{ id: 1, phoneNumber: "22-22-22" }, { id: 2, phoneNumber: "33-33-33" }]
+        phones: [],//{ id: 1, phoneNumber: "22-22-22" }, { id: 2, phoneNumber: "33-33-33" }]
+        newPhone: ""
     };
 
 	componentDidMount() {
@@ -15,7 +16,29 @@ export default class Phone extends React.Component {
                 });
             }        
       )  
-	}
+    }
+
+    handleChange(event) {
+        this.setState({
+            newPhone: event.target.value
+        });
+    }
+
+    addPhone(event) {
+        fetch(window.constants.phones, {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                phoneNumber: this.state.newPhone,
+            }),
+        })
+            .then(function (response) {
+                //return response.json();
+            })
+    }
 
     render() {
         const list = this.state.phones.map((phone, index) => {
@@ -27,6 +50,14 @@ export default class Phone extends React.Component {
                 <ul>
                     {list}
                 </ul>
+                <form onSubmit={this.addPhone}>
+                    <label>
+                        Новый телефон
+                        <input type="text" value={this.state.newPhone} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Добавить" />
+                </form>
+
             </div>
         );
     }
