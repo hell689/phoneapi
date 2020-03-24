bff8bce5c77d9c72dfce38d2507227e7213809a4
Alexey Vladyko <a.uladyka@gmail.com>
2020-03-24 23:37:26 +0300

    PhoneCabinetList, removeCabinetfromPhone add

--- PhoneApi/App/containers/phoneCabinetsTable.jsx
+++ PhoneApi/App/containers/phoneCabinetsTable.jsx
@@ -1,16 +1,38 @@
 ﻿import React from 'react';
 
+function PhoneCabinetList(props) {
+    //props.getPhoneCabinets(props.editedPhone.id);
+    if (props.phoneCabinets == null) {
+        return null;
+    }
+
+    const phoneCabinetList = props.phoneCabinets.map((cabinet) => {
+        return <button type="button" className="btn btn-primary ml-1"
+            key={cabinet.id} onClick={(e) => props.parent.removeCabinetfromPhone(props.editedPhone, cabinet.id, props.parent)}>{cabinet.cabinetNumber}</button>;
+    });
+
+    return (
+        <div className="btn-group btn-group-lg mx-auto" role="group" aria-label="Basic example">
+            {phoneCabinetList}
+        </div>
+    );
+}
+
 export default class PhoneCabinetsTable extends React.Component {
     constructor(props) {
         super(props)
         this.state = {            
-            cabinets: null,
+            cabinets: [],
+            phoneCabinets: [],
             isLoading: false,
         };
+        this.getPhoneCabinets = this.getPhoneCabinets.bind(this);
+        this.removeCabinetfromPhone = this.removeCabinetfromPhone.bind(this);
     }
 
     componentDidMount() {
-        this.getCabinets();
+        this.getCabinets(); 
+        this.getPhoneCabinets(this.props.editedPhone.id);
     }
 
     getCabinets() {
@@ -20,6 +42,18 @@ export default class PhoneCabinetsTable extends React.Component {
             }).then((data) => {
                 this.setState({
                     cabinets: data                    
+                });
+            }
+            )
+    }
+
+    getPhoneCabinets(phoneId) {
+        fetch(window.constants.phones + "/" + phoneId)
+            .then((response) => {
+                return response.json();
+            }).then((data) => {                
+                this.setState({
+                    phoneCabinets: data.cabinets
                 });
             }
             )
@@ -37,43 +71,67 @@ export default class PhoneCabinetsTable extends React.Component {
             })
         })
             .then(function (response) {
-                return response.json();
+                
+                //return response.json();
             });
         event.preventDefault();
+        this.getPhoneCabinets(this.props.editedPhone.id);
+    }
+
+    removeCabinetfromPhone(phone, cabinetId) {
+        this.setState({ isLoading: true });
+        fetch(window.constants.phones + "/" + phone.id + "/" + cabinetId, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(function (response) {
+                return response.json();
+            }).then((data) => {
+                this.getPhoneCabinets(phone.id);
+            }
+            );
     }
 
     render() {
-        if (!this.props.showTable) {
+        /*if (!this.props.showTable) {
             return null;
-        }
-
+        }*/
+        
         const cabinetList = this.state.cabinets.map((cabinet) => {
             return <button type="button" className="btn btn-primary ml-1"
                 key={cabinet.id} onClick={(e) => this.addCabinetToPhone(this.props.editedPhone, cabinet.id)}>{cabinet.cabinetNumber}</button>;
         });
 
         return (
-            <table className="table table-bordered mt-3">
-                <thead>
-                    <tr>
-                        <th scope="col">Кабинеты с номером телефона {this.props.editedPhone.phoneNumber}</th>
-                        <th scope="col">Добавить кабинет</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                               
-                        </td>
-                        <td>
-                            <div className="btn-group btn-group-lg mx-auto" role="group" aria-label="Basic example">
-                                {cabinetList}
-                            </div>
-                        </td>
-                    </tr>
+            <div>
+                <table className="table table-bordered mt-3">
+                    <thead>
+                        <tr>
+                            <th scope="col">Кабинеты с номером телефона {this.props.editedPhone.phoneNumber}</th>
+                            <th scope="col">Добавить кабинет</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <PhoneCabinetList removeCabinetfromPhone={this.removeCabinetfromPhone}
+                                    phoneCabinets={this.state.phoneCabinets}
+                                    editedPhone={this.props.editedPhone} getPhoneCabinets={this.getPhoneCabinets}
+                                    parent={this}/>
+                            </td>
+                            <td>
+                                <div className="btn-group btn-group-lg mx-auto" role="group" aria-label="Basic example">
+                                    {cabinetList}
+                                </div>
+                            </td>
+                        </tr>
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+                <button className="btn btn-primary" onClick={this.props.clickCloseTable}>Закрыть</button>
+                </div>
         );
     }
 }
