4226f952f3394e6ea3e6d4ae6423257d344ac8e4
Alexey Vladyko <a.uladyka@gmail.com>
2020-04-01 23:54:39 +0300

    correct sql

--- PhoneApi/DBRepository/Repositories/PhoneRepository.cs
+++ PhoneApi/DBRepository/Repositories/PhoneRepository.cs
@@ -29,7 +29,7 @@ namespace PhoneApi.DBRepository.Repositories
                 foreach (Phone phone in phones)
                 {
                     List<Cabinet> cabinets = new List<Cabinet>();
-                    List<CabinetPhone> cabinetPhones = context.CabinetPhones.FromSqlRaw("SELECT phoneId, cabinetId FROM CabinetPhone WHERE phoneId = {0}", phone.Id).ToList();
+                    List<CabinetPhone> cabinetPhones = context.CabinetPhones.FromSqlRaw("SELECT Id, phoneId, cabinetId FROM CabinetPhones WHERE phoneId = {0}", phone.Id).ToList();
                     foreach (var cabinetPhone in cabinetPhones)
                     {
                         Cabinet cabinet = context.Cabinets.FirstOrDefault(c => c.Id == cabinetPhone.CabinetId);
@@ -50,7 +50,7 @@ namespace PhoneApi.DBRepository.Repositories
             {
                 Phone phone = await context.Phones.FirstOrDefaultAsync(p => p.Id == phoneId);
                 List<Cabinet> cabinets = new List<Cabinet>();
-                List<CabinetPhone> cabinetPhones = context.CabinetPhones.FromSqlRaw("SELECT phoneId, cabinetId FROM CabinetPhone WHERE phoneId = {0}", phone.Id).ToList();
+                List<CabinetPhone> cabinetPhones = context.CabinetPhones.FromSqlRaw("SELECT Id, phoneId, cabinetId FROM CabinetPhones WHERE phoneId = {0}", phone.Id).ToList();
                 foreach (var cabinetPhone in cabinetPhones)
                 {
                     Cabinet cabinet = context.Cabinets.FirstOrDefault(c => c.Id == cabinetPhone.CabinetId);
@@ -82,7 +82,7 @@ namespace PhoneApi.DBRepository.Repositories
                 cabinetPhone.CabinetId = cabinet.Id;
                 cabinetPhone.Phone = phone;
                 cabinetPhone.PhoneId = phone.Id;
-                context.Database.ExecuteSqlRaw("INSERT INTO CabinetPhone (CabinetId, PhoneId) VALUES ({0}, {1})", cabinetPhone.CabinetId, cabinetPhone.PhoneId);
+                context.Database.ExecuteSqlRaw("INSERT INTO CabinetPhones (CabinetId, PhoneId) VALUES ({0}, {1})", cabinetPhone.CabinetId, cabinetPhone.PhoneId);
                 await context.SaveChangesAsync();
             }
         }
@@ -91,7 +91,7 @@ namespace PhoneApi.DBRepository.Repositories
         {
             using (var context = ContextFactory.CreateDbContext(ConnectionString))
             {
-                context.Database.ExecuteSqlRaw("DELETE FROM CabinetPhone WHERE CabinetId={0} AND PhoneId={1}", cabinet.Id, phone.Id);
+                context.Database.ExecuteSqlRaw("DELETE FROM CabinetPhones WHERE CabinetId={0} AND PhoneId={1}", cabinet.Id, phone.Id);
                 await context.SaveChangesAsync();
             }
         }
