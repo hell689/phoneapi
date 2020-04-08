42c31da5e11cbc72d7ee8590786f37100fc8e9d5
Alexey Vladyko <a.uladyka@gmail.com>
2020-04-08 13:33:40 +0300

    authHelper add

---
+++ PhoneApi/App/containers/authHelper.jsx
@@ -0,0 +1,36 @@
+﻿export default {
+    saveAuth: (login, token) => {
+        sessionStorage.setItem(constants.tokenKey, JSON.stringify({ login: login, access_token: token }));
+    },
+
+    clearAuth: () => {
+        sessionStorage.removeItem(constants.tokenKey);
+    },
+
+    getLogin: () => {
+        let item = sessionStorage.getItem(constants.tokenKey);
+        let login = '';
+        if (item) {
+            login = JSON.parse(item).login;
+        }
+        return login;
+    },
+
+    isLogged: () => {
+        let item = sessionStorage.getItem(constants.tokenKey);
+        if (item) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    getToken: () => {
+        let item = sessionStorage.getItem(constants.tokenKey);
+        let token = null;
+        if (item) {
+            token = JSON.parse(item).access_token;
+        }
+        return token;
+    }
+}
