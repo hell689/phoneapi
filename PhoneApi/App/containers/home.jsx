e0743dc7cc71420592cf695c3e7abaf612606955
Alexey Vladyko <a.uladyka@gmail.com>
2020-04-09 13:37:26 +0300

    extra call getEmployes deleted

--- PhoneApi/App/containers/home.jsx
+++ PhoneApi/App/containers/home.jsx
@@ -36,7 +36,6 @@ export default class Home extends React.Component {
             employees: [],
             isLoading: false,
         };
-        this.getEmployees();
     }
 
     componentDidMount() {
